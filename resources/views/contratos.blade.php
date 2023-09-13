d6d2c6f707b908cd54586bebb23f2d698e19e8a7
José Umaña <jose.umana@procuraduria.gob.pa>
2023-09-13 08:27:47 -0500

    se crea data table para contratos con toda la informacion

--- resources/views/contratos.blade.php
+++ resources/views/contratos.blade.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-    <title>Información de Contratos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>datatables</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
+
 </head>
+
 <body>
 
     <div class="top-bar">
@@ -13,41 +18,72 @@
         <a href="{{ route('personal') }}">Personal</a>
         <a href="{{ route('welcome') }}">Cerrar sesión</a>
     </div>
+<header>
+
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+
+            <table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>Nombre Cliente</th>
+                <th>Apellido Cliente</th>
+                <th>Cedula Cliente</th>
+                <th>Nombre Personal</th>
+                <th>Apellido Personal</th>
+                <th>Cedula Personal</th>
+ 		        <th>Número Contrato</th>
+                <th>Costo</th>
+                <th>Fecha Evento</th>
+                <th>Lugar Evento</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($contratos as $contrato)
+            <tr>
+                <td>{{ $contrato['nombreCliente'] }}</td>
+                <td>{{ $contrato['apellidoCliente'] }}</td>
+                <td>{{ $contrato['cedulaCliente'] }}</td>
+                <td>{{ $contrato['nombrePersonal'] }}</td>
+                <td>{{ $contrato['apellidoPersonal'] }}</td>
+                <td>{{ $contrato['cedulaPersonal'] }}</td>
+                <td>{{ $contrato['numContrato'] }}</td>
+                <td>{{ $contrato['costo'] }}</td>
+                <td>{{ $contrato['fechaEvento'] }}</td>
+                <td>{{ $contrato['lugarEvento'] }}</td>
+            </tr>
+            @endforeach
+            
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Nombre Cliente</th>
+                <th>Apellido Cliente</th>
+                <th>Cedula Cliente</th>
+                <th>Nombre Personal</th>
+                <th>Apellido Personal</th>
+                <th>Cedula Personal</th>
+ 		        <th>Número Contrato</th>
+                <th>Costo</th>
+                <th>Fecha Evento</th>
+                <th>Lugar Evento</th>
+            </tr>
+        </tfoot>
+    </table>
 
 
-<table border="1">
-    <tr>
-        <th>Nombre Cliente</th>
-        <th>Apellido Cliente</th>
-        <th>Cédula Cliente</th>
-        <th>Nombre Personal</th>
-        <th>Apellido Personal</th>
-        <th>Cédula Personal</th>
-        <th>Número Contrato</th>
-        <th>Costo</th>
-        <th>Fecha Evento</th>
-        <th>Lugar Evento</th>
-    </tr>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
 
-    
-    @foreach($contratos as $contrato)
-    <tr>
-        <td>{{ $contrato['nombreCliente'] }}</td>
-        <td>{{ $contrato['apellidoCliente'] }}</td>
-        <td>{{ $contrato['cedulaCliente'] }}</td>
-        <td>{{ $contrato['nombrePersonal'] }}</td>
-        <td>{{ $contrato['apellidoPersonal'] }}</td>
-        <td>{{ $contrato['cedulaPersonal'] }}</td>
-        <td>{{ $contrato['numContrato'] }}</td>
-        <td>{{ $contrato['costo'] }}</td>
-        <td>{{ $contrato['fechaEvento'] }}</td>
-        <td>{{ $contrato['lugarEvento'] }}</td>
-    </tr>
-@endforeach
+<script>
+    new DataTable('#example');
+</script>
 
 
-
-</table>
 
 </body>
 </html>
