944f0cfb4ca2126d624b3abcfceaf7d317be242e
José Umaña <jose.umana@procuraduria.gob.pa>
2023-09-19 15:35:12 -0500

    ultimo error corregido

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContratoController;
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('welcome');
+
+
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
+
+
+Route::get('/registro', function () {
+    return view('create');
+})->name('registro');
+
+
+Route::get('/registroContratos', function () {
+    return view('registroContratos');
+})->name('registroContratos');
+
+Route::get('/personal', function () {
+    return view('personal');
+})->name('personal');
+
+Route::get('/login', function () {
+    return view('admin');
+})->name('admin');
+
+Route::get('contratos', [ContratoController::class, 'mostrarInformacion'])->name('contratos');
